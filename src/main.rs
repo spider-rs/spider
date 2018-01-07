7d83f5d1d8af6b3c77c9d9716075ab2ef86f1178
madeindjs <contact@rousseau-alexandre.fr>
2018-01-07 17:14:31 +0100

    Follow example
    from https://hyper.rs/guides/client/basic/

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate futures;
+extern crate hyper;
+extern crate tokio_core;
+
+use std::io::{self, Write};
+use futures::{Future, Stream};
+use hyper::Client;
+use tokio_core::reactor::Core;
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut core = Core::new().unwrap();
+    let client = Client::new(&core.handle());
+
+    let uri = "http://localhost:4000".parse().unwrap();
+    let work = client.get(uri).and_then(|res| {
+        println!("Response: {}", res.status());
+
+        res.body().for_each(|chunk| {
+            io::stdout().write_all(&chunk).map_err(From::from)
+        })
+    });
+    core.run(work).unwrap();
 }
