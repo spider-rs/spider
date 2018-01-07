34727a81f0166b4b27f539b4748415e3e7da0170
madeindjs <contact@rousseau-alexandre.fr>
2018-01-07 18:43:40 +0100

    Improve organization

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,15 @@
 extern crate reqwest;
 extern crate scraper;
 
-use std::io::Read;
-use scraper::{Html, Selector};
+mod website;
 
-/// Launch an HTTP GET query to te given URL & parse body response content
-fn get(url: &str) -> Html {
-    let mut res = reqwest::get(url).unwrap();
-    let mut body = String::new();
-    res.read_to_string(&mut body).unwrap();
 
-    Html::parse_document(&body)
-}
+use website::Website;
 
 
 fn main() {
-    let body: Html = get("http://localhost:4000");
-    let selector = Selector::parse("a").unwrap();
+    let mut localhost = Website::new("http://localhost:4000");
+    localhost.crawl();
 
-    for element in body.select(&selector) {
-        assert_eq!("a", element.value().name());
-        println!("{:?}", element.value());
-    }
+    println!("{:?}", localhost);
 }
---
+++ src/website.rs
@@ -0,0 +1,40 @@
+use std::io::Read;
+use scraper::{Html, Selector};
+use reqwest;
+
+#[derive(Debug)]
+pub struct Website {
+    domain: String,
+    links: Vec<String>,
+}
+
+impl Website {
+    pub fn new(domain: &str) -> Self {
+        Self {
+            domain: domain.to_string(),
+            links: Vec::new(),
+        }
+    }
+
+    pub fn crawl(&mut self) {
+        let body: Html = Self::get(&self.domain);
+        let selector = Selector::parse("a").unwrap();
+
+        for element in body.select(&selector) {
+
+            match element.value().attr("href") {
+                Some(href) => self.links.push(href.to_string()),
+                None => (),
+            };
+        }
+    }
+
+    /// Launch an HTTP GET query to te given URL & parse body response content
+    fn get(url: &str) -> Html {
+        let mut res = reqwest::get(url).unwrap();
+        let mut body = String::new();
+        res.read_to_string(&mut body).unwrap();
+
+        Html::parse_document(&body)
+    }
+}
