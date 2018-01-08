dbfa78a5c297106936e7fde3cff488f220d2f0b4
madeindjs <contact@rousseau-alexandre.fr>
2018-01-08 07:44:37 +0100

    Add page struct

--- src/website.rs
+++ src/website.rs
@@ -6,75 +6,87 @@ use reqwest;
 #[derive(Debug)]
 pub struct Website {
     domain: String,
-    links: Vec<Link>,
+    links: Vec<String>,
+    pages: Vec<Page>,
 }
 
 impl Website {
     /// Initialize Website object with one link
     pub fn new(domain: &str) -> Self {
         // create home link
-        let mut links: Vec<Link> = Vec::new();
-        links.push(Link::new(domain, "/"));
+        let mut links: Vec<String> = Vec::new();
+        links.push(format!("{}/", domain));
 
         Self {
             domain: domain.to_string(),
             links: links,
+            pages: Vec::new(),
         }
     }
 
     pub fn crawl(&mut self) {
+        let mut new_links: Vec<String> = Vec::new();
 
-        let mut new_links: Vec<Link> = Vec::new();
+        for link in &self.links {
+            let page = Page::new(link);
+            let mut links_founded = page.links(&self.domain);
 
-        for mut link in &self.links {
-            let body: Html = link.visit();
-            let selector = Selector::parse("a").unwrap();
+            new_links.append(&mut links_founded);
 
-            for element in body.select(&selector) {
-
-                match element.value().attr("href") {
-                    Some(href) => {
-
-                        // Keep only links for this domains
-                        match href.find('/') {
-                            Some(0) => new_links.push(Link::new(&self.domain, href)),
-                            Some(_) => (),
-                            None => (),
-                        };
-                    }
-                    None => (),
-                };
-            }
-
+            self.pages.push(page);
         }
 
-        self.links.append(&mut new_links);
+        self.links = new_links;
     }
 }
 
 /// Represent a link who can be visited
 #[derive(Debug)]
-struct Link {
+struct Page {
     url: String,
-    visited: bool,
+    html: Html,
 }
 
-impl Link {
-    fn new(domain: &str, url: &str) -> Self {
+impl Page {
+    fn new(url: &str) -> Self {
+        let html = Self::visit(url);
+
         Self {
-            url: format!("{}{}", domain, url),
-            visited: false,
+            url: url.to_string(),
+            html: html,
         }
     }
 
     /// Launch an HTTP GET query to te given URL & parse body response content
-    fn visit(&self) -> Html {
-        let mut res = reqwest::get(&self.url).unwrap();
+    fn visit(url: &str) -> Html {
+        let mut res = reqwest::get(url).unwrap();
         let mut body = String::new();
         res.read_to_string(&mut body).unwrap();
 
-        // todo: set link visited
-
         Html::parse_document(&body)
+    }
+
+    fn links(&self, domain: &str) -> Vec<String> {
+        let mut urls: Vec<String> = Vec::new();
+
+        let selector = Selector::parse("a").unwrap();
+
+        for element in self.html.select(&selector) {
+
+            match element.value().attr("href") {
+                Some(href) => {
+
+                    // Keep only links for this domains
+                    match href.find('/') {
+                        Some(0) => urls.push(format!("{}{}", domain, href)),
+                        Some(_) => (),
+                        None => (),
+                    };
+                }
+                None => (),
+            };
+        }
+
+        urls
     }
 }
