f6ef109eeee6f5154f180fa1279a12f05a255d77
j-mendez <jeff@a11ywatch.com>
2022-04-20 16:54:57 -0400

    bench(go): add go lang crolly bench

--- benches/crawl.rs
+++ benches/crawl.rs
@@ -2,17 +2,24 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use std::process::{Command};
 use std::time::Duration;
 pub mod node_crawler;
+pub mod go_crolly;
 
 /// bench spider crawling between different libs
 pub fn bench_speed(c: &mut Criterion) {
     let node_crawl_script = node_crawler::gen_crawl();
+    let go_crawl_script = go_crolly::gen_crawl();
     let mut group = c.benchmark_group("crawl-speed");
-    
+        
     group.sample_size(10).measurement_time(Duration::new(85, 0) + Duration::from_millis(500));
     group.bench_function("Rust[spider]: with crawl 10 times", |b| b.iter(||Command::new("spider")
         .args(["--delay", "0", "--domain", "https://rsseau.fr", "crawl"])
         .output()
         .expect("rust command failed to start")
+    ));
+    group.bench_function("Go[crolly]: with crawl 10 times", |b| b.iter(||Command::new("go")
+        .arg(&go_crawl_script)
+        .output()
+        .expect("go command failed to start")
     ));
     group.bench_function("Node.js[crawler]: with crawl 10 times", |b| b.iter(|| Command::new("node")
         .arg(&node_crawl_script)
---
+++ benches/go_crolly.rs
@@ -0,0 +1,44 @@
+use std::fs::File;
+use std::io::{BufWriter, Write};
+
+pub fn crawl_stub() -> String {
+    r#"
+    package main
+
+    import (
+        "fmt"
+    
+        "github.com/gocolly/colly/v2"
+    )
+    
+    func main() {
+        c := colly.NewCollector()
+    
+        c.OnHTML(`a[href^="/"]:not([href$=".png"]):not([href$=".jpg"]):not([href$=".mp4"]):not([href$=".mp3"]):not([href$=".gif"]),
+        a[href^="https://rsseau.fr"]:not([href$=".png"]):not([href$=".jpg"]):not([href$=".mp4"]):not([href$=".mp3"]):not([href$=".gif"])`, func(e *colly.HTMLElement) {
+            e.Request.Visit(e.Attr("href"))
+        })
+    
+        c.OnRequest(func(r *colly.Request) {
+            fmt.Println("- visiting ", r.URL)
+        })
+    
+        c.Visit("https://rsseau.fr")
+    }
+    "#.to_string()
+}
+
+pub fn gen_crawl() -> String {
+    let crawl_script = String::from("./go-crolly.go");
+    let file = File::create(&crawl_script).expect("create go crawl script");
+    let mut file = BufWriter::new(file);
+    let stub = crawl_stub();
+    file.write_all(stub.as_bytes()).expect("Unable to write data");
+
+    crawl_script
+}
+
+
+
+
+
