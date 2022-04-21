31b1700b5405b80f276d585df4573bca3fed7a2b
j-mendez <jeff@a11ywatch.com>
2022-04-21 16:29:47 -0400

    bench(minor): add black box usage

--- benches/crawl.rs
+++ benches/crawl.rs
@@ -1,4 +1,4 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, black_box, criterion_main, Criterion};
 use std::process::{Command};
 use std::time::Duration;
 pub mod node_crawler;
@@ -6,26 +6,26 @@ pub mod go_crolly;
 
 /// bench spider crawling between different libs
 pub fn bench_speed(c: &mut Criterion) {
-    let mut group = c.benchmark_group("crawl-speed");
+    let mut group = c.benchmark_group("crawl-speed/libraries");
     let node_crawl_script = node_crawler::gen_crawl();
     let go_crawl_script = go_crolly::gen_crawl();
         
     group.sample_size(10).measurement_time(Duration::new(180, 0) + Duration::from_millis(500));
-    group.bench_function("Rust[spider]: with crawl 10 times", |b| b.iter(||Command::new("spider")
+    group.bench_function("Rust[spider]: with crawl 10 times", |b| b.iter(||black_box(Command::new("spider")
         .args(["--delay", "0", "--domain", "https://rsseau.fr", "crawl"])
         .output()
-        .expect("rust command failed to start")
+        .expect("rust command failed to start"))
     ));
-    group.bench_function("Go[crolly]: with crawl 10 times", |b| b.iter(||Command::new("go")
+    group.bench_function("Go[crolly]: with crawl 10 times", |b| b.iter(||black_box(Command::new("go")
         .arg("run")
         .arg(&go_crawl_script)
         .output()
-        .expect("go command failed to start")
+        .expect("go command failed to start"))
     ));
-    group.bench_function("Node.js[crawler]: with crawl 10 times", |b| b.iter(|| Command::new("node")
+    group.bench_function("Node.js[crawler]: with crawl 10 times", |b| b.iter(|| black_box(Command::new("node")
         .arg(&node_crawl_script)
         .output()
-        .expect("node command failed to start")
+        .expect("node command failed to start"))
     ));
     group.finish();
 }
