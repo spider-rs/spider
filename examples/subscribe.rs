de08acb655ef8e70c5f8e44f21835f7801eaf538
j-mendez <jeff@a11ywatch.com>
2023-08-27 13:18:29 -0400

    chore(sync): fix initial link sub capture

--- examples/subscribe.rs
+++ examples/subscribe.rs
@@ -9,13 +9,11 @@ async fn main() {
     let mut website: Website = Website::new("https://rsseau.fr");
     let mut rx2 = website.subscribe(16).unwrap();
 
-    let join_handle = tokio::spawn(async move {
+    tokio::spawn(async move {
         while let Ok(res) = rx2.recv().await {
             println!("{:?}", res.get_url());
         }
     });
 
     website.crawl().await;
-
-    let _ = join_handle.await.unwrap();
 }
