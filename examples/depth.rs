fe14802030336e225b025b18105ccd5292145373
j-mendez <jeff@a11ywatch.com>
2023-12-19 15:11:01 -0500

    feat(depth): add crawl depth level control

---
+++ examples/depth.rs
@@ -0,0 +1,33 @@
+//! `cargo run --example depth --features budget`
+extern crate spider;
+
+use spider::tokio;
+use spider::website::Website;
+use std::io::Error;
+use std::time::Instant;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let mut website = Website::new("https://rsseau.fr")
+        .with_depth(3)
+        .build()
+        .unwrap();
+
+    let start = Instant::now();
+    website.crawl().await;
+    let duration: std::time::Duration = start.elapsed();
+
+    let links = website.get_links();
+
+    for link in links {
+        println!("- {:?}", link.as_ref());
+    }
+
+    println!(
+        "Time elapsed in website.crawl() is: {:?} for total pages: {:?}",
+        duration,
+        links.len()
+    );
+
+    Ok(())
+}
