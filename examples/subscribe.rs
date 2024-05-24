b02e0919246ed206b0d88f60427b65d1e19c2b9f
j-mendez <jeff@a11ywatch.com>
2024-05-24 07:17:59 -0400

    chore(examples): fix subscribe stdout end sequence

--- examples/subscribe.rs
+++ examples/subscribe.rs
@@ -10,25 +10,31 @@ async fn main() {
     let mut website: Website = Website::new("https://rsseau.fr");
     let mut rx2: tokio::sync::broadcast::Receiver<spider::page::Page> =
         website.subscribe(0).unwrap();
+    let mut stdout = tokio::io::stdout();
 
-    tokio::spawn(async move {
-        let mut stdout = tokio::io::stdout();
-
+    let join_handle = tokio::spawn(async move {
         while let Ok(res) = rx2.recv().await {
             let _ = stdout
                 .write_all(format!("- {}\n", res.get_url()).as_bytes())
                 .await;
         }
+        stdout
     });
 
     let start = std::time::Instant::now();
     website.crawl().await;
     website.unsubscribe();
     let duration = start.elapsed();
+    let mut stdout = join_handle.await.unwrap();
 
-    println!(
-        "Time elapsed in website.crawl() is: {:?} for total pages: {:?}",
-        duration,
-        website.get_links().len()
-    )
+    let _ = stdout
+        .write_all(
+            format!(
+                "Time elapsed in website.crawl() is: {:?} for total pages: {:?}",
+                duration,
+                website.get_links().len()
+            )
+            .as_bytes(),
+        )
+        .await;
 }
