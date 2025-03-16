75683a3b9f7e06d7c5352c2cd7c520cc18efbdef
j-mendez <jeff@a11ywatch.com>
2025-03-15 22:34:24 -0400

    chore(utils): fix DocumentSelectors serde compile error

--- spider_utils/src/lib.rs
+++ spider_utils/src/lib.rs
@@ -9,7 +9,6 @@ use tokio_stream::StreamExt;
 
 /// The type of selectors that can be used to query.
 #[derive(Default, Debug, Clone)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct DocumentSelectors<K> {
     /// CSS Selectors.
     pub css: HashMap<K, Vec<Selector>>,
