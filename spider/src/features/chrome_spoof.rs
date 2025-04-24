b8b6a141df970d463a64ce8c95aef61009e8c8fd
j-mendez <jeff@a11ywatch.com>
2025-04-24 18:29:14 -0400

    chore(chrome): fix missing bitness

--- spider/src/features/chrome_spoof.rs
+++ spider/src/features/chrome_spoof.rs
@@ -14,6 +14,8 @@ pub struct HighEntropyUaData {
     pub architecture: String,
     /// The device model (mostly non-empty for Android devices).
     pub model: String,
+    /// The bitness property.
+    pub bitness: String,
     /// The platform being used.
     pub platform: String,
     /// The OS platform version (e.g., "10.0" for Windows 10, "13" for Android 13).
@@ -34,89 +36,100 @@ pub fn build_high_entropy_data(
         .and_then(|s| s.strip_prefix("Chrome/"))
         .unwrap_or("135.0.0.0");
 
-    let (architecture, model, platform, platform_version): (String, String, String, String) =
-        if user_agent.contains("Android") {
-            let version = user_agent
-                .split(';')
-                .find(|s| s.trim().starts_with("Android "))
-                .and_then(|s| s.trim().strip_prefix("Android "))
-                .unwrap_or("13")
-                .to_string();
+    let (architecture, model, platform, platform_version, bitness): (
+        &str,
+        String,
+        &str,
+        String,
+        &str,
+    ) = if user_agent.contains("Android") {
+        let version = user_agent
+            .split(';')
+            .find_map(|s| s.trim().strip_prefix("Android "))
+            .unwrap_or("13");
 
-            let model = user_agent
-                .split(';')
-                .nth(2)
-                .map(|s| s.trim().to_string())
-                .unwrap_or_default();
+        let model = user_agent
+            .split(';')
+            .nth(2)
+            .map(|s| s.trim().to_string())
+            .unwrap_or_default();
 
-            ("arm".to_string(), model, "Android".to_string(), version)
-        } else if user_agent.contains("Windows NT") {
-            let version = user_agent
-                .split("Windows NT ")
-                .nth(1)
-                .and_then(|s| s.split(';').next())
-                .unwrap_or("10.0")
-                .to_string();
-
-            (
-                "x86".to_string(),
-                "".to_string(),
-                "Windows".to_string(),
-                version,
-            )
-        } else if user_agent.contains("Mac OS X") {
-            let chrome_major = full_version
-                .split('.')
-                .next()
-                .and_then(|s| s.parse::<u32>().ok())
-                .unwrap_or(135);
-
-            // Start at 14.6.1 for Chrome 135
-            let base_chrome = 135;
-            let base_mac = 14.6;
-
-            let delta = if chrome_major > base_chrome {
-                ((chrome_major - base_chrome) as f32 * 0.1).round()
-            } else {
-                0.0
-            };
-
-            let mac_major = base_mac + delta;
-            let platform_version = format!("{:.1}.1", mac_major);
-
-            (
-                "arm".to_string(),
-                "".to_string(),
-                "macOS".to_string(),
-                platform_version,
-            )
-        } else if user_agent.contains("Linux") {
-            let platform_version = full_version
-                .split('.')
-                .take(3)
-                .collect::<Vec<_>>()
-                .join(".");
-
-            (
-                "x86".to_string(),
-                "".to_string(),
-                "Linux".to_string(),
-                platform_version,
-            )
+        let bitness = if user_agent.contains("arm64") || user_agent.contains("aarch64") {
+            "64"
         } else {
-            (
-                "x86".to_string(),
-                "".to_string(),
-                "Unknown".to_string(),
-                "1.0.0".to_string(),
-            )
+            "32"
         };
 
+        ("arm", model, "Android", version.to_string(), bitness)
+    } else if user_agent.contains("Windows NT") {
+        let version = user_agent
+            .split("Windows NT ")
+            .nth(1)
+            .and_then(|s| s.split(';').next())
+            .unwrap_or("10.0");
+
+        let bitness = if user_agent.contains("Win64")
+            || user_agent.contains("x64")
+            || user_agent.contains("WOW64")
+        {
+            "64"
+        } else {
+            "32"
+        };
+
+        (
+            "x86",
+            "".to_string(),
+            "Windows",
+            version.to_string(),
+            bitness,
+        )
+    } else if user_agent.contains("Mac OS X") {
+        let chrome_major = full_version
+            .split('.')
+            .next()
+            .and_then(|s| s.parse::<u32>().ok())
+            .unwrap_or(135);
+
+        let base_chrome = 135;
+        let base_mac = 14.6;
+        let delta = if chrome_major > base_chrome {
+            ((chrome_major - base_chrome) as f32 * 0.1).round()
+        } else {
+            0.0
+        };
+
+        let mac_major = base_mac + delta;
+        let platform_version = format!("{:.1}.1", mac_major);
+
+        ("arm", "".to_string(), "macOS", platform_version, "64")
+    } else if user_agent.contains("Linux") {
+        let platform_version = full_version
+            .split('.')
+            .take(3)
+            .collect::<Vec<_>>()
+            .join(".");
+
+        let bitness = if user_agent.contains("x86_64")
+            || user_agent.contains("amd64")
+            || user_agent.contains("arm64")
+        {
+            "64"
+        } else {
+            "32"
+        };
+
+        ("x86", "".to_string(), "Linux", platform_version, bitness)
+    } else {
+        ("x86", "".to_string(), "Unknown", "1.0.0".to_string(), "64")
+    };
+
     HighEntropyUaData {
-        architecture: architecture.into(),
+        architecture: architecture.to_string(),
+        bitness: bitness.to_string(),
         model,
-        platform: platform.into(),
-        platform_version: platform_version.into(),
+        platform: platform.to_string(),
+        platform_version,
         full_version_list: vec![
             BrandEntry {
                 brand: "Google Chrome".into(),
@@ -124,7 +137,7 @@ pub fn build_high_entropy_data(
             },
             BrandEntry {
                 brand: "Not-A.Brand".into(),
-                version: "8.0.0.0".into(), // todo: dynamic parse.
+                version: "8.0.0.0".into(),
             },
             BrandEntry {
                 brand: "Chromium".into(),
@@ -155,9 +168,10 @@ pub fn spoof_user_agent_data_high_entropy_values(data: &HighEntropyUaData) -> St
         .join(",");
 
     format!(
-        "(()=>{{const v={{brands:[{brands}],mobile:!1,platform:'{platform}',getHighEntropyValues:h=>Promise.resolve(Object.fromEntries(h.map(k=>[k,{{architecture:'{arch}',model:'{model}',platformVersion:'{plat}',fullVersionList:[{full_versions}]}}[k]??null])))}};const f=function(){{return v}};Object.defineProperty(f,'toString',{{value:()=>\"function get userAgentData() {{ [native code] }}\"}});Object.defineProperty(Navigator.prototype,'userAgentData',{{get:f,configurable:!0}});}})();",
+        "(()=>{{const v={{brands:[{brands}],mobile:!1,platform:'{platform}',getHighEntropyValues:h=>Promise.resolve(Object.fromEntries(h.map(k=>[k,{{architecture:'{arch}',model:'{model}',bitness:'{bitness}',platformVersion:'{plat}',fullVersionList:[{full_versions}]}}[k]??null])))}};const f=function(){{return v}};Object.defineProperty(f,'toString',{{value:()=>\"function get userAgentData() {{ [native code] }}\"}});Object.defineProperty(Navigator.prototype,'userAgentData',{{get:f,configurable:!0}});}})();",
         platform = data.platform,
         arch = data.architecture,
+        bitness = data.bitness,
         model = data.model,
         plat = data.platform_version,
         brands = brands,
